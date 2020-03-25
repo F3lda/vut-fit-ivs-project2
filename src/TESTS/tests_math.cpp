50c83634380ae72f21f3f85a5fd76fcd344fa641
MichalPysik <n3r0x541@gmail.com>
2020-03-25 13:28:48 +0100

    zprehledneni hiearchie (slozek a souboru) knihoven a testu

--- src/TESTS/tests_math.cpp
+++ src/TESTS/tests_math.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
-#include "basic_math.h"
-#include "adv_math.h"
+#include "../basic_math.h"
+#include "../adv_math.h"
 
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
--- src/compile_tests.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-c++ basic_math.cpp adv_math.cpp basic_math.h adv_math.h tests_math.cpp
-
-exit 0
