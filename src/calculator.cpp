b87a9d176cdbb20cea8db9c3b7c9f4bb821fcee2
F3lda <kajel.jirgl@gmail.com>
2020-04-01 14:27:33 +0200

    GUI setup update #0.2

--- src/calculator.cpp
+++ src/calculator.cpp
@@ -137,7 +137,9 @@ void CreateWindow(GtkApplication *app)
 void on_button_clicked(GtkButton *button, gpointer user_data)
 {
 	const char *buttonLabel = gtk_button_get_label(button);
+	const char *buttonName = gtk_widget_get_name(GTK_WIDGET(button));
 	g_print("Button: %s\n", buttonLabel);
+	g_print("Name: %s\n", buttonName);
 	
 	if(strcmp(buttonLabel, "10 + 20") == 0){
 		double result = add(10, 20);
@@ -153,6 +155,35 @@ void on_button_clicked(GtkButton *button, gpointer user_data)
 	//on_window_destroy(button, user_data);
 }
 
+
+
+int on_hover(GtkWidget *button, gpointer user_data)
+{
+	g_print("hover\n");
+	gtk_style_context_add_class(gtk_widget_get_style_context(button),"najete");
+	return TRUE;
+}
+
+int on_leave(GtkWidget *button, gpointer user_data)
+{
+	g_print("leave\n");
+	gtk_style_context_remove_class(gtk_widget_get_style_context(button),"najete");
+	return TRUE;
+}
+
+int on_click(GtkWidget *button, gpointer user_data)
+{
+	g_print("click\n");
+	gtk_style_context_add_class(gtk_widget_get_style_context(button),"zmackle");
+	return TRUE;
+}
+
+int on_release(GtkWidget *button, gpointer user_data)
+{
+	g_print("release\n");
+	gtk_style_context_remove_class(gtk_widget_get_style_context(button),"zmackle");
+	return TRUE;
+}
 /*
 ** MAIN
 *********************************************************
@@ -162,44 +193,64 @@ void mainSetup(GtkApplication *app, GtkWidget *window, GtkBuilder *builder)
 	// test button
 	GtkWidget *button;
 	button = GTK_WIDGET(gtk_builder_get_object(builder, "button1"));
+	gtk_style_context_add_class(gtk_widget_get_style_context(button),"tlacidlo");
+	gtk_button_set_relief(GTK_BUTTON(button), GTK_RELIEF_NONE);
 	g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), app);
+	g_signal_connect(button, "enter-notify-event", G_CALLBACK(on_hover), NULL);
+	g_signal_connect(button, "leave-notify-event", G_CALLBACK(on_leave), NULL);
+	g_signal_connect(button, "button-press-event", G_CALLBACK(on_click), NULL);
+	g_signal_connect(button, "button-release-event", G_CALLBACK(on_release), NULL);
 	
+
+	//TEXT VIEW
+	GtkTextBuffer *buffer;
+	GtkTextMark *mark;
+	GtkTextIter iter;
+	const char *text = "TEXT";
+
+	buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(gtk_builder_get_object(builder, "textview1")));
+	//text = gtk_entry_get_text(GTK_ENTRY(w->entry));
+	
+	mark = gtk_text_buffer_get_insert(buffer);
+	gtk_text_buffer_get_iter_at_mark(buffer, &iter, mark);
+
+	/* Insert newline (only if there's already text in the buffer). */
+	if (gtk_text_buffer_get_char_count(buffer))
+	    gtk_text_buffer_insert(buffer, &iter, "\n", 1);
+
+	gtk_text_buffer_insert(buffer, &iter, text, -1);
+
+
+
+
+
+
 	button = GTK_WIDGET(gtk_builder_get_object(builder, "button2"));
+	gtk_button_set_relief(GTK_BUTTON(button), GTK_RELIEF_NONE);	
+	gtk_widget_set_name(button, "tlacidlo2");
 	g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), app);
 
+
+
 	button = GTK_WIDGET(gtk_builder_get_object(builder, "button3"));
-	gtk_widget_set_name(button, "tlacidlo");
+	gtk_widget_set_name(button, "tlacidlo3");
 	g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), app);
 	
 	
-	
-	// CSS
-	GtkCssProvider *cssProvider = gtk_css_provider_new();
-	const char *CSS_DATA = "GtkButton#tlacidlo {border-radius: 0; color: red;}";
-	GError *gerror = NULL;
-	gtk_css_provider_load_from_data(cssProvider, CSS_DATA, strlen(CSS_DATA), &gerror);
-	if(gerror != NULL){
-		g_warning("ERROR loading CSS: %s", gerror->message);
-		g_free(gerror);
-	}
-	
-	//gtk_css_provider_load_from_file(cssProvider, "styles.css", NULL);
-	gtk_style_context_add_provider_for_screen(gdk_screen_get_default(), GTK_STYLE_PROVIDER(cssProvider), GTK_STYLE_PROVIDER_PRIORITY_USER); // _PRIORITY_APPLICATION
-	
+	//https://elementaryos.stackexchange.com/questions/15936/button-css-not-working-in-juno	
 }
 /* ********************************************************
 ******************************************************** */
 
 static void Activate(GtkApplication *app, gpointer userData)
 {
+	GError *gerror;
 	GtkWidget *window;
 	GtkBuilder *builder;
 	
-	// Construct a GtkBuilder instance and load our UI description
-	builder = gtk_builder_new();
-
 	// Load UI from file. If error occurs, report it and quit application.
-	GError *gerror = NULL;
+	builder = gtk_builder_new();
+	gerror = NULL;
 	//const char *XML_DATA = "";
 	//gtk_builder_add_from_string(builder, XML_DATA, strlne(XML_DATA), &gerror);
 	if(!gtk_builder_add_from_file(builder, FILE_UI, &gerror)){
@@ -207,6 +258,20 @@ static void Activate(GtkApplication *app, gpointer userData)
 		g_free(gerror);
 		return;
 	}
+
+	// CSS for UI
+	GtkCssProvider *cssProvider = gtk_css_provider_new();
+	gerror = NULL;
+	//const char *CSS_DATA = "";	
+	//gtk_css_provider_load_from_data(cssProvider, CSS_DATA, strlen(CSS_DATA), &gerror);
+	gtk_css_provider_load_from_path(cssProvider, "calculator_ui_styles.css", &gerror);
+	if(gerror != NULL){
+		g_warning("ERROR loading CSS: %s", gerror->message);
+		g_free(gerror);
+	}
+	gtk_style_context_add_provider_for_screen(gdk_screen_get_default(), GTK_STYLE_PROVIDER(cssProvider), GTK_STYLE_PROVIDER_PRIORITY_USER); // _PRIORITY_APPLICATION
+	g_object_unref(cssProvider);
+
 
 	// Get main window pointer from UI
 	window = GTK_WIDGET(gtk_builder_get_object(builder, WIDGET_WINDOW));
@@ -216,7 +281,7 @@ static void Activate(GtkApplication *app, gpointer userData)
 	// Setup window
 	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
 	gtk_window_set_default_size(GTK_WINDOW(window), 480, 380);
-	gtk_window_set_title(GTK_WINDOW(window), "Calculator COGFIT-19-20");
+	gtk_window_set_title(GTK_WINDOW(window), "Calculator - COGFIT1920");
 	// Connect signal handlers to the widgets
 	g_signal_connect(window, "destroy", G_CALLBACK(on_window_destroy), NULL);
 	g_signal_connect(window, "delete_event", G_CALLBACK(on_window_close), NULL);  
@@ -231,7 +296,7 @@ static void Activate(GtkApplication *app, gpointer userData)
 	// Destroy builder, since we don't need it anymore
 	g_object_unref(builder);
 	// Show window. All other widgets are automatically shown by GtkBuilder
-	gtk_widget_show(window);
+	gtk_widget_show_all(window);
 }
 
 int main(int argc, char *argv[])
