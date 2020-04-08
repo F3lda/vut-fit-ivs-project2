7c4fd41de89c716e677279b6d5a2e2349cf13f53
F3lda <kajel.jirgl@gmail.com>
2020-04-08 19:22:06 +0200

    GUI setup done (buttons, textview, etc. done; prepared for backend)

--- src/calculator.cpp
+++ src/calculator.cpp
@@ -9,25 +9,27 @@
 
 
 /*
-** App is finished. Clean up everything.
+** MAIN WINDOW CALLBACKS
+*********************************************************
 *********************************************************/
+
+// App is finished. Clean up everything.
+//*********************************************************
 void on_window_destroy(GtkWidget *widget, gpointer user_data)
 {
 	g_print("Done.\n");
 }
 
-/*
-** When closing window
-*********************************************************/
+// When closing window
+//*********************************************************
 int on_window_close(GtkWidget *widget, gpointer user_data)
 {
 	//return TRUE;	// Returning TRUE stops the window being deleted.
 	return FALSE;	// Returning FALSE allows deletion.   
 }
 
-/*
-** When key is pressed
-*********************************************************/
+// When key is pressed
+//*********************************************************
 void on_key_press(GtkWidget *widget, GdkEventKey *event, gpointer user_data)
 {
 	char buffer[100];
@@ -35,9 +37,8 @@ void on_key_press(GtkWidget *widget, GdkEventKey *event, gpointer user_data)
 	g_print("Key pressed: %s\n",buffer);
 }
 
-/*
-** When key is released
-*********************************************************/
+// When key is released
+//*********************************************************
 void on_key_release(GtkWidget *widget, GdkEventKey *event, gpointer user_data)
 {
 	char buffer[100];
@@ -45,111 +46,54 @@ void on_key_release(GtkWidget *widget, GdkEventKey *event, gpointer user_data)
 	g_print("Key pressed: %s\n",buffer);
 }
 
-/*
-** Show question dialog
-*********************************************************/
-int show_question(const char *title, const char *message, GtkApplication *app)
-{
-/*
-	GTK_RESPONSE_NONE         = -1,
-	GTK_RESPONSE_REJECT       = -2,
-	GTK_RESPONSE_ACCEPT       = -3,
-	GTK_RESPONSE_DELETE_EVENT = -4,
-	GTK_RESPONSE_OK           = -5,
-	GTK_RESPONSE_CANCEL       = -6,
-	GTK_RESPONSE_CLOSE        = -7,
-	GTK_RESPONSE_YES          = -8,
-	GTK_RESPONSE_NO           = -9,
-	GTK_RESPONSE_APPLY        = -10,
-	GTK_RESPONSE_HELP         = -11
-*/
-	GtkWindow *parentWindow = gtk_application_get_active_window(app);
-	GtkWidget *dialog;
-	dialog = gtk_message_dialog_new(parentWindow,
-		GTK_DIALOG_DESTROY_WITH_PARENT,
-		GTK_MESSAGE_QUESTION,
-		GTK_BUTTONS_YES_NO,
-		"%s", message);
-	gtk_window_set_title(GTK_WINDOW(dialog), title);
-	// keep window on top
-	gtk_window_set_modal(GTK_WINDOW(dialog), TRUE);
-	gtk_window_set_transient_for(GTK_WINDOW(dialog), parentWindow);
-	
-	// show dialog
-	gtk_application_add_window(app, GTK_WINDOW(dialog));
-	int response = gtk_dialog_run(GTK_DIALOG(dialog));
-	gtk_application_remove_window(app, GTK_WINDOW(dialog));
-	gtk_widget_destroy(dialog);
-	
-	return response;
-}
-
-
-/*
-** Child Window Example
-*********************************************************
-*********************************************************/
-void onDestroyWindow(GtkWidget *widget, GtkApplication *app)
-{
-	GtkWindow *parentWindow = gtk_application_get_active_window(app);
-	gtk_application_remove_window(app, parentWindow);
-	g_print("Child window destroyed!\n");
-}
-/*
-** Create window
-*********************************************************/
-void CreateWindow(GtkApplication *app)
-{
-	GtkWindow *parentWindow = gtk_application_get_active_window(app);
-	GtkWidget *window;
-	GtkWidget *button;
-
-	// Setup window
-	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	// application add window
-	gtk_application_add_window(app, GTK_WINDOW(window));
-	gtk_window_set_title(GTK_WINDOW(window), "Tooltip");
-	gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
-	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER_ON_PARENT);
-	// keep window on top
-	gtk_window_set_modal(GTK_WINDOW(window), TRUE);
-	gtk_window_set_transient_for(GTK_WINDOW(window), parentWindow);
-	gtk_window_set_destroy_with_parent(GTK_WINDOW(window), TRUE);
-	
-	// Customize window
-	gtk_container_set_border_width(GTK_CONTAINER(window), 15);
-
-	button = gtk_button_new_with_label("Button");
-	gtk_widget_set_tooltip_text(button, "Button widget");
-
-	gtk_widget_set_halign(button, GTK_ALIGN_START);
-	gtk_widget_set_valign(button, GTK_ALIGN_START);
-
-	gtk_container_add(GTK_CONTAINER(window), button);
-	
-	
-	g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(onDestroyWindow), app);
-	gtk_widget_show_all(window);
-}
-
 
 /*
 ** FUNCTIONS
 *********************************************************
 *********************************************************/
+void gtk_textview_append_text(GtkTextView *text_view, char * text)
+{
+	GtkTextBuffer *buffer;
+	GtkTextIter start, end;
 
+	buffer = gtk_text_view_get_buffer(text_view);
+	if (gtk_text_buffer_get_char_count(buffer))
+		gtk_text_buffer_insert_at_cursor(buffer, "\n", 1);
+	
+	gtk_text_buffer_insert_at_cursor(buffer, text, strlen(text));
+}
 
+void gtk_textview_set_text(GtkTextView *text_view, char * text)
+{
+	GtkTextBuffer *buffer;
+	GtkTextIter start, end;
 
+	buffer = gtk_text_view_get_buffer(text_view);
+	gtk_text_buffer_set_text(buffer, text, strlen(text));
+}
 
-/* Callback for the buttons */
-void on_button_clicked(GtkButton *button, gpointer user_data)
+char * gtk_textview_get_text(GtkTextView *text_view)
+{
+	GtkTextBuffer *buffer;
+	GtkTextIter start, end;
+	char *text;
+
+	buffer = gtk_text_view_get_buffer(text_view);
+	gtk_text_buffer_get_bounds(buffer, &start, &end);
+	text = gtk_text_buffer_get_text(buffer, &start, &end, TRUE);
+	return text; //need to free text
+}
+
+// Callback for the buttons
+//*********************************************************
+void on_button_click(GtkButton *button, gpointer user_data)
 {
 	const char *buttonLabel = gtk_button_get_label(button);
 	const char *buttonName = gtk_widget_get_name(GTK_WIDGET(button));
 	g_print("Button: %s\n", buttonLabel);
 	g_print("Name: %s\n", buttonName);
 	
-	if(strcmp(buttonLabel, "10 + 20") == 0){
+	/*if(strcmp(buttonLabel, "10 + 20") == 0){
 		double result = add(10, 20);
 		g_print("10 + 20 = %f\n", result);
 	} else if(strcmp(buttonLabel, "Close") == 0){
@@ -158,62 +102,115 @@ void on_button_clicked(GtkButton *button, gpointer user_data)
 		if(result == GTK_RESPONSE_YES){
 			CreateWindow((GtkApplication *)user_data);
 		}
-	}
+	}*/
 	
 	//on_window_destroy(button, user_data);
 }
 
-
-
-int on_hover(GtkWidget *button, gpointer user_data)
+/*
+** CALLBACKS
+*********************************************************
+*********************************************************/
+int on_button_hover(GtkWidget *button, gpointer user_data)
 {
 	g_print("hover\n");
 	gtk_style_context_add_class(gtk_widget_get_style_context(button),"hover");
 	return TRUE;
 }
 
-int on_leave(GtkWidget *button, gpointer user_data)
+int on_button_leave(GtkWidget *button, gpointer user_data)
 {
 	g_print("leave\n");
 	gtk_style_context_remove_class(gtk_widget_get_style_context(button),"hover");
 	return TRUE;
 }
 
-int on_press(GtkWidget *button, gpointer user_data)
+int on_button_press(GtkWidget *button, gpointer user_data)
 {
 	g_print("click\n");
 	gtk_style_context_add_class(gtk_widget_get_style_context(button),"active");
-	on_button_clicked(GTK_BUTTON(button), user_data);
+	on_button_click(GTK_BUTTON(button), user_data);
 	return TRUE;
 }
 
-int on_release(GtkWidget *button, gpointer user_data)
+int on_button_release(GtkWidget *button, gpointer user_data)
 {
 	g_print("release\n");
 	gtk_style_context_remove_class(gtk_widget_get_style_context(button),"active");
 	return TRUE;
 }
 
-void on_icon_click(GtkEntry *entry, int icon_pos)
-{
-	g_print("icon: %d\n",icon_pos);
-}
+
 
 void on_textview_selection(GtkTextView *text_view)
 {
-
 	GtkTextBuffer *buffer = gtk_text_view_get_buffer(text_view);
-	GtkTextIter start, end;	
+	GtkTextIter start, end;
 	if(gtk_text_buffer_get_selection_bounds(buffer, &start, &end)){
-		//g_print("selection\n");
-		//g_print("selection line: %d\n",gtk_text_iter_get_line(&start));
-		//g_print("%d %d\n",gtk_text_iter_get_offset(&start)-gtk_text_iter_get_line_offset(&start),gtk_text_iter_get_chars_in_line(&start));
-
-		gtk_text_iter_set_line_offset(&start,0);
-		gtk_text_iter_set_line_offset(&end,gtk_text_iter_get_chars_in_line(&start)-1);
+		gtk_text_iter_set_line_offset(&start, 0);
+		gtk_text_iter_set_line_offset(&end, gtk_text_iter_get_chars_in_line(&end)-1);
+		if(!gtk_text_iter_ends_line(&end))
+			gtk_text_iter_set_line_offset(&end, gtk_text_iter_get_chars_in_line(&end));
 		char *text = gtk_text_buffer_get_text(buffer, &start, &end, TRUE);
 		g_print("selection: %s\n",text);
 		free(text);
+	}
+}
+
+int on_textview_cursor_move_timeout(GtkTextView *text_view)
+{
+	GtkTextBuffer *buffer = gtk_text_view_get_buffer(text_view);
+	GtkTextMark *mark = gtk_text_buffer_get_insert(buffer);
+	GtkTextIter start, end;
+	gtk_text_buffer_get_iter_at_mark(buffer, &start, mark);
+	
+	end = start;
+	gtk_text_iter_set_line_offset(&start, 0);
+	gtk_text_iter_set_line_offset(&end, gtk_text_iter_get_chars_in_line(&end)-1);
+	if(!gtk_text_iter_ends_line(&end))
+		gtk_text_iter_set_line_offset(&end, gtk_text_iter_get_chars_in_line(&end));
+	char *text = gtk_text_buffer_get_text(buffer, &start, &end, TRUE);
+	g_print("cursor move: %d\n", gtk_text_iter_get_line(&start));
+	g_print("selection: %s\n",text);
+	free(text);
+}
+
+void on_textview_cursor_move(GtkTextView *text_view)
+{
+	g_timeout_add(33, (int (*)(void*))G_CALLBACK(on_textview_cursor_move_timeout), text_view);
+	//guint threadID =
+	//g_source_remove(threadID); // stop timeout
+}
+
+void on_textview_resize(GtkTextView *text_view, GdkRectangle *allocation, gpointer user_data)
+{
+	g_print("resize\n");
+	g_print("%d %d %d %d\n",allocation->x, allocation->y, allocation->width, allocation->height);
+	// auto scroll to bottom
+	GtkTextBuffer *buffer;
+	GtkTextIter iter;
+	GtkTextMark *mark;
+
+	buffer = gtk_text_view_get_buffer(text_view);
+	gtk_text_buffer_get_end_iter(buffer, &iter);
+	gtk_text_iter_set_line_offset(&iter, 0);// Move the iterator to the beginning of line, so we don't scroll in horizontal direction 
+	mark = gtk_text_buffer_create_mark(buffer, NULL, &iter, FALSE);
+	gtk_text_view_scroll_mark_onscreen(text_view, mark);
+	gtk_text_buffer_delete_mark(buffer, mark);
+}
+
+void on_entry_icon_click(GtkEntry *entry, int icon_pos, GdkEvent *event, GtkTextView *text_view)
+{
+	g_print("icon: %d\n",icon_pos);
+	if(icon_pos == 0){
+		GtkTextBuffer *buffer = gtk_text_view_get_buffer(text_view);
+		GtkTextMark *mark = gtk_text_buffer_get_insert(buffer);
+		GtkTextIter iter;
+		gtk_text_buffer_get_iter_at_mark(buffer, &iter, mark);
+		gtk_text_iter_backward_line(&iter);
+		gtk_text_buffer_place_cursor(buffer, &iter);
+		gtk_text_view_scroll_mark_onscreen(text_view, mark);
+		on_textview_cursor_move_timeout(text_view);
 	}
 }
 
@@ -223,65 +220,69 @@ void on_textview_selection(GtkTextView *text_view)
 *********************************************************/
 void mainSetup(GtkApplication *app, GtkWidget *window, GtkBuilder *builder)
 {
+	// SETUP CALLBACKS
+	//*********************************************************
 	GtkWidget *widget;
+
 	//BUTTONS
 	widget = GTK_WIDGET(gtk_builder_get_object(builder, "button23"));
 	gtk_widget_set_name(widget, "button_result");
 	gtk_style_context_add_class(gtk_widget_get_style_context(widget),"button_result");
 	gtk_button_set_relief(GTK_BUTTON(widget), GTK_RELIEF_NONE);
-	g_signal_connect(widget, "clicked", G_CALLBACK(on_button_clicked), app);
-	g_signal_connect(widget, "enter-notify-event", G_CALLBACK(on_hover), NULL);
-	g_signal_connect(widget, "leave-notify-event", G_CALLBACK(on_leave), NULL);
-	g_signal_connect(widget, "button-press-event", G_CALLBACK(on_press), NULL);
-	g_signal_connect(widget, "button-release-event", G_CALLBACK(on_release), NULL);
+	g_signal_connect(widget, "clicked", G_CALLBACK(on_button_click), app);
+	g_signal_connect(widget, "enter-notify-event", G_CALLBACK(on_button_hover), NULL);
+	g_signal_connect(widget, "leave-notify-event", G_CALLBACK(on_button_leave), NULL);
+	g_signal_connect(widget, "button-press-event", G_CALLBACK(on_button_press), NULL);
+	g_signal_connect(widget, "button-release-event", G_CALLBACK(on_button_release), NULL);
 	
 	int i = 0;
 	while(i < 25){
 		char tempBuffer[24];
 		sprintf(tempBuffer, "button%d", ++i);
 		widget = GTK_WIDGET(gtk_builder_get_object(builder, tempBuffer));
-		g_signal_connect(widget, "enter-notify-event", G_CALLBACK(on_hover), NULL);
-		g_signal_connect(widget, "leave-notify-event", G_CALLBACK(on_leave), NULL);
-		g_signal_connect(widget, "button-press-event", G_CALLBACK(on_press), NULL);
-		g_signal_connect(widget, "button-release-event", G_CALLBACK(on_release), NULL);
+		g_signal_connect(widget, "enter-notify-event", G_CALLBACK(on_button_hover), NULL);
+		g_signal_connect(widget, "leave-notify-event", G_CALLBACK(on_button_leave), NULL);
+		g_signal_connect(widget, "button-press-event", G_CALLBACK(on_button_press), NULL);
+		g_signal_connect(widget, "button-release-event", G_CALLBACK(on_button_release), NULL);
 	}
 
+	//ENTRY on icon click
+	widget = GTK_WIDGET(gtk_builder_get_object(builder, "entry1"));
+	g_signal_connect(widget, "icon-press", G_CALLBACK(on_entry_icon_click), GTK_TEXT_VIEW(GTK_WIDGET(gtk_builder_get_object(builder, "textview1"))));
 
+	//TEXT VIEW on row selection
+	widget = GTK_WIDGET(gtk_builder_get_object(builder, "textview1"));
+	g_signal_connect(widget, "extend-selection", G_CALLBACK(on_textview_selection), NULL);
+	//TEXT VIEW text auto scroll
+	g_signal_connect(widget, "size-allocate", G_CALLBACK(on_textview_resize), NULL);
+	//TEXT VIEW on cursor move
+	g_signal_connect(widget, "grab-focus", G_CALLBACK(on_textview_cursor_move), NULL);
+	//TEXT VIEW on cursor move by arrows
+	g_signal_connect(widget, "move-cursor", G_CALLBACK(on_textview_cursor_move), NULL);
+
+
+
+	// EXAMPLES
+	//*********************************************************
 	//ENTRY
 	widget = GTK_WIDGET(gtk_builder_get_object(builder, "entry1"));
 	gtk_entry_set_text(GTK_ENTRY(widget), "sdfgdgfdfgf dg df input\nsdf sdf sdfsddddd input text");
-	//ENTRY on icon clicked	
-	g_signal_connect(widget, "icon-press", G_CALLBACK(on_icon_click), NULL);
+	
 
 	//TEXT VIEW
 	widget = GTK_WIDGET(gtk_builder_get_object(builder, "textview1"));
 	
-	GtkTextBuffer *buffer;
-	const char *text;
+	const char *text = "ahoj jak se mas\n lol dobre";
+	gtk_textview_set_text(GTK_TEXT_VIEW(widget), (char *)text);
 
-	buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(widget));
-	text = gtk_entry_get_text(GTK_ENTRY(GTK_WIDGET(gtk_builder_get_object(builder, "entry1"))));
-	
-	gtk_text_buffer_set_text(buffer, text, strlen(text));
-	
-	if (gtk_text_buffer_get_char_count(buffer))
-		gtk_text_buffer_insert_at_cursor(buffer, "\n", 1);
-	
-	gtk_text_buffer_insert_at_cursor(buffer, text, strlen(text));
+	char *text2 = gtk_textview_get_text(GTK_TEXT_VIEW(widget));
+	g_print("text: %s\n",text2);
+	free(text2);
 
+	const char *text3 = "sdfgdgfdfgf dg df input\nsdf sdf sdfsddddd input text\nsdfgdgfdfgf dg df input\nsdf sdf sdfsddddd input text\nsdfgdgfdfgf dg df input\nsdf sdf sdfsddddd input text\nsdfgdgfdfgf dg df input\nsdf sdf sdfsddddd input text\nsdfgdgfdfgf dg df input\nsdf sdf sdfsddddd input text\nsdfgdgfdfgf dg df input\nsdf sdf sdfsddddd input text\nsdfgdgfdfgf dg df input\nsdf sdf sdfsddddd input text\nsdfgdgfdfgf dg df input\nsdf sdf sdfsddddd input text\nsdfgdgfdfgf dg df input\nsdf sdf sdfsddddd input text\nsdfgdgfdfgf dg df input\nsdf sdf sdfsddddd input text END";
 
-	GtkTextIter start, end;
-	gtk_text_buffer_get_bounds(buffer, &start, &end);
-	text = gtk_text_buffer_get_text(buffer, &start, &end, TRUE);
-
-	gtk_entry_set_text(GTK_ENTRY(GTK_WIDGET(gtk_builder_get_object(builder, "entry1"))), text);
-	free((char *)text);
-	//TEXT VIEW on get selected
-	g_signal_connect(widget, "extend-selection", G_CALLBACK(on_textview_selection), NULL);
-
-	
-	
-	
+	gtk_textview_append_text(GTK_TEXT_VIEW(widget), (char *)text3);
+	gtk_textview_append_text(GTK_TEXT_VIEW(widget), (char *)text);
 }
 /* ********************************************************
 ******************************************************** */
@@ -351,3 +352,99 @@ int main(int argc, char *argv[])
 	g_object_unref(app);
 	return status;
 }
+
+
+
+
+
+/*
+** UNUSED CODE EXAMPLES
+*********************************************************
+*********************************************************/
+/*
+//*
+//* Show question dialog
+//*********************************************************
+int show_question(const char *title, const char *message, GtkApplication *app)
+{
+	//GTK_RESPONSE_NONE         = -1,
+	//GTK_RESPONSE_REJECT       = -2,
+	//GTK_RESPONSE_ACCEPT       = -3,
+	//GTK_RESPONSE_DELETE_EVENT = -4,
+	//GTK_RESPONSE_OK           = -5,
+	//GTK_RESPONSE_CANCEL       = -6,
+	//GTK_RESPONSE_CLOSE        = -7,
+	//GTK_RESPONSE_YES          = -8,
+	//GTK_RESPONSE_NO           = -9,
+	//GTK_RESPONSE_APPLY        = -10,
+	//GTK_RESPONSE_HELP         = -11
+
+	GtkWindow *parentWindow = gtk_application_get_active_window(app);
+	GtkWidget *dialog;
+	dialog = gtk_message_dialog_new(parentWindow,
+		GTK_DIALOG_DESTROY_WITH_PARENT,
+		GTK_MESSAGE_QUESTION,
+		GTK_BUTTONS_YES_NO,
+		"%s", message);
+	gtk_window_set_title(GTK_WINDOW(dialog), title);
+	// keep window on top
+	gtk_window_set_modal(GTK_WINDOW(dialog), TRUE);
+	gtk_window_set_transient_for(GTK_WINDOW(dialog), parentWindow);
+	
+	// show dialog
+	gtk_application_add_window(app, GTK_WINDOW(dialog));
+	int response = gtk_dialog_run(GTK_DIALOG(dialog));
+	gtk_application_remove_window(app, GTK_WINDOW(dialog));
+	gtk_widget_destroy(dialog);
+	
+	return response;
+}
+
+
+//*
+//* Child Window Example
+//*********************************************************
+//*********************************************************
+void onDestroyWindow(GtkWidget *widget, GtkApplication *app)
+{
+	GtkWindow *parentWindow = gtk_application_get_active_window(app);
+	gtk_application_remove_window(app, parentWindow);
+	g_print("Child window destroyed!\n");
+}
+//*
+//* Create window
+//*********************************************************
+void CreateWindow(GtkApplication *app)
+{
+	GtkWindow *parentWindow = gtk_application_get_active_window(app);
+	GtkWidget *window;
+	GtkWidget *button;
+
+	// Setup window
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	// application add window
+	gtk_application_add_window(app, GTK_WINDOW(window));
+	gtk_window_set_title(GTK_WINDOW(window), "Tooltip");
+	gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
+	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER_ON_PARENT);
+	// keep window on top
+	gtk_window_set_modal(GTK_WINDOW(window), TRUE);
+	gtk_window_set_transient_for(GTK_WINDOW(window), parentWindow);
+	gtk_window_set_destroy_with_parent(GTK_WINDOW(window), TRUE);
+	
+	// Customize window
+	gtk_container_set_border_width(GTK_CONTAINER(window), 15);
+
+	button = gtk_button_new_with_label("Button");
+	gtk_widget_set_tooltip_text(button, "Button widget");
+
+	gtk_widget_set_halign(button, GTK_ALIGN_START);
+	gtk_widget_set_valign(button, GTK_ALIGN_START);
+
+	gtk_container_add(GTK_CONTAINER(window), button);
+	
+	
+	g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(onDestroyWindow), app);
+	gtk_widget_show_all(window);
+}
+*/
