8e8d3edca9f0df148f23525f48285a268b1c4d10
Thanh Quang Tran <michaeltran262@gmail.com>
2020-04-19 15:32:55 +0200

    Rewamped for stdin from terminal

--- src/stddev.cpp
+++ src/stddev.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <iostream>
 #include "adv_math.h"
 #include "basic_math.h"
@@ -11,36 +12,44 @@ int main()
     double number, count = 0, ariSum = 0, ariMean, devSum = 0, stdDev, tmp;
     char whiteSpace;
 
-    //Pro výpočet sumy čísel ze stdin
-    while(scanf("%lf%c", &number, &whiteSpace)!=EOF)
+    if (isatty(0)) 
     {
-        //Aritmeticky prumer
-        ariSum = add(ariSum, number);
-        count++;
-        //
+        printf("Input was not redirected.\n");
+        return -1;
     }
-
-    if(count != 0)
+    else
     {
-        ariMean = divide(ariSum, count, 0);
+        //Výpočet sumy čísel ze stdin
+        while(scanf("%lf%c", &number, &whiteSpace)!=EOF)
+        {
+            //Aritmeticky prumer
+            ariSum = add(ariSum, number);
+            count++;
+            //
+        }
+
+        if(count != 0)
+        {
+            ariMean = divide(ariSum, count, 0);
+        }
+
+        //printf("Aritmeticky prumer: %lf\n", ariMean);
+
+        rewind(stdin);
+
+        //Pro výpočet čitatele rozptylu
+        while(scanf("%lf%c", &number, &whiteSpace)!=EOF)
+        {
+            tmp = sub(number, ariMean);
+            tmp = Exponent(tmp, 2, 0.000001, 0);
+            devSum = add(devSum, tmp);
+        }
+
+        //Směrodatná odchylka
+        stdDev = NthRoot(divide(devSum, count, 0), 2, 0.000001, 0);
+
+        //printf("Smerodatna odchylka:\nCitatel: %lf\nJmenovatel: %.0lf\nSměrodatná odchylka: %0.5lf\n", devSum, count, stdDev);
+
+        return 0;
     }
-
-    //printf("Aritmeticky prumer: %lf\n", ariMean);
-
-    rewind(stdin);
-
-    //Pro výpočet čitatele rozptylu
-    while(scanf("%lf%c", &number, &whiteSpace)!=EOF)
-    {
-        tmp = sub(number, ariMean);
-        tmp = Exponent(tmp, 2, 0.000001, 0);
-        devSum = add(devSum, tmp);
-    }
-
-    //Směrdatná odchylka
-    stdDev = NthRoot(divide(devSum, count, 0), 2, 0.000001, 0);
-
-    //printf("Smerodatna odchylka:\nCitatel: %lf\nJmenovatel: %.0lf\nSměrodatná odchylka: %0.5lf/n", devSum, count, stdDev);
-
-    return 0;
 }
